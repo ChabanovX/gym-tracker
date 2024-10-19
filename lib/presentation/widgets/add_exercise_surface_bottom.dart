544f1882660f50668d62bc48faa8a70c0a9c57fe
ChabanovX <firstchabanov@gmail.com>
2024-10-19 20:04:00 +0300

    test

--- lib/presentation/widgets/add_exercise_surface_bottom.dart
+++ lib/presentation/widgets/add_exercise_surface_bottom.dart
@@ -1,8 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 
 import 'stopwatch.dart';
 import '../../style/style.dart';
-
 
 class AddExerciseSurfaceBottom extends StatelessWidget {
   final Widget popUpSurface;
@@ -16,36 +17,32 @@ class AddExerciseSurfaceBottom extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BackgroundStyles.gradientDecorationReversed(context),
-      // color: CupertinoColors.inactiveGray,
-      child: Row(
-        children: [
-          Expanded(
-            child: Center(
-              child: StopwatchWidget(
-                stopwatch: stopwatch,
-              ),
+    return Row(
+      children: [
+        Expanded(
+          child: Center(
+            child: StopwatchWidget(
+              stopwatch: stopwatch,
             ),
           ),
-          Expanded(
-            child: Center(
-              child: CupertinoButton(
-                child: const Icon(
-                  CupertinoIcons.add_circled,
-                  size: 64,
-                ),
-                onPressed: () {
-                  showCupertinoModalPopup(
-                    context: context,
-                    builder: (BuildContext context) => popUpSurface,
-                  );
-                },
+        ),
+        Expanded(
+          child: Center(
+            child: CupertinoButton(
+              child: const Icon(
+                CupertinoIcons.add_circled,
+                size: 64,
               ),
+              onPressed: () {
+                showCupertinoModalPopup(
+                  context: context,
+                  builder: (BuildContext context) => popUpSurface,
+                );
+              },
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
